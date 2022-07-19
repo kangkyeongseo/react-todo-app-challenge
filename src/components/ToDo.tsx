177a9a59ddece7a3ef2c21924aeeddc50b90db38
kangkyeongseo <kks_big@naver.com>
2022-07-19 17:24:48 +0900

    Change Category

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,14 +1,32 @@
-interface IProp {
-  text: string;
-}
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IProp) {
+function ToDo({ text, id, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: string) => {
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((todo) => todo.id === id);
+      const newToDo = { text, id, category: event as any };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
